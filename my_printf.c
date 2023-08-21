aeb4d0f5cf7ac78ec8f023c2b7081eb671414fa5
Mlotshwa99 <mlotshwadineo080@gmail.com>
2023-08-20 21:14:40 -0700

    my_printf.c

--- my_printf.c
+++ my_printf.c
@@ -1,80 +1,59 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include "main.h"
 
-/**
- * _printf - Custom printf function
- * @format: The format string
- * @...: Arguments to be printed
- *
- * Return: Number of characters printed
- */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+const char *ptr = format;
+int charCount = 0;
 
-	va_start(args, format);
+va_list args;
+va_start(args, format);
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-					count += _putchar(va_arg(args, int));
-					break;
-				case 's':
-					count += _print_str(va_arg(args, char *));
-					break;
-				case '%':
-					count += _putchar('%');
-					break;
-				default:
-					_putchar('%');
-					_putchar(*format);
-					count += 2;
-					break;
-			}
-		}
-		else
-		{
-			count += _putchar(*format);
-		}
-		format++;
-	}
-
-	va_end(args);
-	return count;
-}
-
-/**
- * _putchar - Print a single character to stdout
- * @c: The character to be printed
- *
- * Return: 1 (success)
- */
-int _putchar(char c)
+while (*ptr != '\0')
 {
-	return write(1, &c, 1);
-}
-
-/**
- * _print_str - Print a string to stdout
- * @str: The string to be printed
- *
- * Return: Number of characters printed
- */
-int _print_str(char *str)
+if (*ptr == '%')
 {
-	int count = 0;
-	while (*str)
-	{
-		count += _putchar(*str);
-		str++;
-	}
-	return count;
+ptr++; /* Move past the '%'*/
+
+if (*ptr == 'c')
+{
+char ch = va_arg(args, int);
+putchar(ch);
+charCount++;
+}
+else if (*ptr == 's')
+{
+char *str = va_arg(args, char*);
+while (*str != '\0')
+{
+putchar(*str);
+str++;
+charCount++;
+}
+}
+else if (*ptr == '%')
+{
+putchar('%');
+charCount++;
+}
+}
+else
+{
+putchar(*ptr);
+charCount++;
+}
+ptr++;
+}
+va_end(args);
+
+return (charCount);
 }
 
+int main()
+{
+_printf("Character: %c\n", 'A');
+_printf("String: %s\n", "Hello, world!");
+_printf("Literal percentage sign: %%\n");
+
+return (0);
+}
