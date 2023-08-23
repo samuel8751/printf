7fec895291289b376608ad2e375e1cc14f8c1b73
samuel8751 <samuelgithinji041@gmail.com>
2023-08-23 03:21:02 +0300

    updated

--- my_printf.c
+++ my_printf.c
@@ -1,80 +1,55 @@
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
-* _printf - Custom printf function
-* @format: The format string
-* @...: Arguments to be printed
-*
-* Return: Number of characters printed
-*/
+ * _printf - Custom printf implementation
+ * @format: Format string
+ * Return: Number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
+	va_list args;
+	int count = 0;
 
-va_start(args, format);
+	va_start(args, format);
 
-while (*format)
-{
-if (*format == '%')
-{
-format++;
-switch (*format)
-{
-case 'c':
-count += _putchar(va_arg(args, int));
-break;
-case 's':
-count += _print_str(va_arg(args, char *));
-break;
-case '%':
-count += _putchar('%');
-break;
-default:
-_putchar('%');
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					count += printf("%s", va_arg(args, const char *));
+					break;
+				case 'd':
+				case 'i':
+					count += printf("%d", va_arg(args, int));
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+				default:
+					/* Handle unsupported specifier */
+					break;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
 
-_putchar(*format);
-count += 2;
-break;
-}
-}
-else
-{
-count += _putchar(*format);
-}
-format++;
-}
-
-va_end(args);
-return (count);
-}
-
-/**
-* _putchar - Print a single character to stdout
-* @c: The character to be printed
-*
-* Return: 1 (success)
-*/
-int _putchar(char c)
-{
-return (write(1, &c, 1));
+	va_end(args);
+	return (count);
 }
 
-/**
-* _print_str - Print a string to stdout
-* @str: The string to be printed
-*
-* Return: Number of characters printed
-*/
-int _print_str(char *str)
-{
-int count = 0;
-while (*str)
-{
-count += _putchar(*str);
-str++;
-}
-return (count);
-}
