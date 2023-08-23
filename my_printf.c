679deab4ad90068844491e82d3aae8441db20449
samuel8751 <samuelgithinji041@gmail.com>
2023-08-23 05:06:13 +0300

    updated

--- my_printf.c
+++ my_printf.c
@@ -3,71 +3,74 @@
 #include "main.h"
 
 /**
+ * _printf - Custom printf implementation
+ * @format: Format string
+ * @...: Arguments to be printed
+ *
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                    putchar(va_arg(args, int));
+                    count++;
+                    break;
+                case 's':
+                    count += printf("%s", va_arg(args, const char *));
+                    break;
+                case 'd':
+                case 'i':
+                    count += printf("%d", va_arg(args, int));
+                    break;
+                case 'b':
+                {
+                    unsigned int num = va_arg(args, unsigned int);
+                    print_binary(num, &count);
+                    break;
+                }
+                case '%':
+                    putchar('%');
+                    count++;
+                    break;
+                default:
+                    /* Handle unsupported specifier */
+                    break;
+            }
+        }
+        else
+        {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
+
+    va_end(args);
+    return count;
+}
+
+/**
  * print_binary - Prints the binary representation of an unsigned int
  * @n: The number to print in binary
  * @count: Pointer to count variable to update the count
  */
 void print_binary(unsigned int n, int *count)
 {
-	if (n > 1)
-		print_binary(n / 2, count);
-	*count += _putchar('0' + (n % 2));
-}
-
-/**
- * _printf - Custom printf implementation
- * @format: Format string
- * Return: Number of characters printed
- */
-int _printf(const char *format, ...)
-{
-	va_list args;
-	int count = 0;
-
-	va_start(args, format);
-
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-					_putchar(va_arg(args, int));
-					count++;
-					break;
-				case 's':
-					count += _print_str(va_arg(args, char *));
-					break;
-				case 'd':
-				case 'i':
-					count += printf("%d", va_arg(args, int));
-					break;
-				case 'b':
-					print_binary(va_arg(args, unsigned int), &count);
-					break;
-				case '%':
-					_putchar('%');
-					count++;
-					break;
-				default:
-					_putchar('%');
-					_putchar(*format);
-					count += 2;
-					break;
-			}
-		}
-		else
-		{
-			_putchar(*format);
-			count++;
-		}
-		format++;
-	}
-
-	va_end(args);
-	return (count);
+    if (n > 1)
+        print_binary(n / 2, count);
+    *count += putchar('0' + (n % 2));
 }
 
 /**
@@ -78,7 +81,7 @@ int _printf(const char *format, ...)
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+    return (write(1, &c, 1));
 }
 
 /**
@@ -89,12 +92,12 @@ int _putchar(char c)
  */
 int _print_str(char *str)
 {
-	int count = 0;
-	while (*str)
-	{
-		count += _putchar(*str);
-		str++;
-	}
-	return (count);
+    int count = 0;
+    while (*str)
+    {
+        count += _putchar(*str);
+        str++;
+    }
+    return count;
 }
 
