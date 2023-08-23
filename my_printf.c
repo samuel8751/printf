1ab9ad1c69b2580b4ed0b048bb53fe600b107146
samuel8751 <samuelgithinji041@gmail.com>
2023-08-23 04:49:27 +0300

    updated

--- my_printf.c
+++ my_printf.c
@@ -3,6 +3,18 @@
 #include "main.h"
 
 /**
+ * print_binary - Prints the binary representation of an unsigned int
+ * @n: The number to print in binary
+ * @count: Pointer to count variable to update the count
+ */
+void print_binary(unsigned int n, int *count)
+{
+	if (n > 1)
+		print_binary(n / 2, count);
+	*count += _putchar('0' + (n % 2));
+}
+
+/**
  * _printf - Custom printf implementation
  * @format: Format string
  * Return: Number of characters printed
@@ -22,34 +34,67 @@ int _printf(const char *format, ...)
 			switch (*format)
 			{
 				case 'c':
-					putchar(va_arg(args, int));
+					_putchar(va_arg(args, int));
 					count++;
 					break;
 				case 's':
-					count += printf("%s", va_arg(args, const char *));
+					count += _print_str(va_arg(args, char *));
 					break;
 				case 'd':
 				case 'i':
 					count += printf("%d", va_arg(args, int));
 					break;
+				case 'b':
+					print_binary(va_arg(args, unsigned int), &count);
+					break;
 				case '%':
-					putchar('%');
+					_putchar('%');
 					count++;
 					break;
 				default:
-					/* Handle unsupported specifier */
+					_putchar('%');
+					_putchar(*format);
+					count += 2;
 					break;
 			}
 		}
 		else
 		{
-			putchar(*format);
+			_putchar(*format);
 			count++;
 		}
 		format++;
 	}
 
 	va_end(args);
+	return (count);
+}
+
+/**
+ * _putchar - Print a single character to stdout
+ * @c: The character to be printed
+ *
+ * Return: 1 (success)
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _print_str - Print a string to stdout
+ * @str: The string to be printed
+ *
+ * Return: Number of characters printed
+ */
+int _print_str(char *str)
+{
+	int count = 0;
+	while (*str)
+	{
+		count += _putchar(*str);
+		str++;
+	}
 	return (count);
 }
 
